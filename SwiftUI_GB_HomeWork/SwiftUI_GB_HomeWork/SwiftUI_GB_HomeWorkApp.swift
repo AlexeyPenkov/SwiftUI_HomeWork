4ea001de6b8958080d266946932c17cf3ccd1273
Алексей Пеньков <alexey_penkov@list.ru>
2021-11-26 15:04:59 +0800

    initial commit for SwiftUI home work

---
+++ SwiftUI_GB_HomeWork/SwiftUI_GB_HomeWork/SwiftUI_GB_HomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GB_HomeWorkApp.swift
+//  SwiftUI_GB_HomeWork
+//
+//  Created by Алексей Пеньков on 26.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GB_HomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
